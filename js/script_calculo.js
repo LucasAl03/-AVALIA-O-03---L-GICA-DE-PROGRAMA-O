c797a7fd046c5c481a5d3184ff6090d643188e68
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-26 19:54:32 -0300

    arquivo script_calculo.js sofreu leves modificações nas respostas e recebeu a adição do valor adicional em reais

--- js/script_calculo.js
+++ js/script_calculo.js
@@ -2,15 +2,15 @@ const valorAdd = (objLote) =>{
     let resposta = ''
 
     if((objLote.valor) <= 20000){
-        resposta = `Isento`
+        resposta = `Isento de valor adicional`
     }else if((objLote.valor > 20000) && (objLote.valor <= 100000)){
-        resposta = `Valor adicional de 5%`
+        resposta = `Valor adicional de 5% em torno de R$ ${(parseFloat(objLote.valor) * 0.05).toFixed(2).replace('.',',')} reais`
     }else if((objLote.valor > 100000) && (objLote.valor <= 500000)){
-        resposta = `Valor adicional de 10%`
+        resposta = `Valor adicional de 10% em torno de R$ ${(parseFloat(objLote.valor) * 0.1).toFixed(2).replace('.',',')} reais`
     }else if((objLote.valor > 500000) && (objLote.valor <= 1000000)){
-        resposta = `Valor adiconal de 15%`
+        resposta = `Valor adiconal de 15% em torno de R$ ${(parseFloat(objLote.valor) * 0.15).toFixed(2).replace('.',',')} reais`
     }else{
-        resposta = `Valor adicional de 20%`
+        resposta = `Valor dicional de 20%, em torno de R$ ${(parseFloat(objLote.valor) * 0.2).toFixed(2).replace('.',',')} reais`
     }
 
     return resposta
